51bb4d6a88ebc38bc4c480344c0b8dab71c57501
n1k0din <nik726@gmail.com>
2021-09-02 11:11:21 +0200

    Add form

--- movie/views.py
+++ movie/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
+from django.db.models import Q
 
 from movie.models import KinopoiskMovie
 
 
-def index(request):
+def get_random_similar_movies(movie, amount=3, delta_years=7):
+    target_year = movie.year
+    min_year, max_year = target_year - delta_years, target_year + delta_years
+
+    options = KinopoiskMovie.objects\
+        .exclude(id=movie.id)\
+        .filter(Q(year__gt=min_year) & Q(year__lt=max_year))\
+        .order_by('?')[:amount]
+
+    return options
+
+
+def get_movie_and_options():
     random_movie = KinopoiskMovie.objects.order_by('?').first()
+    options = get_random_similar_movies(random_movie)
 
-    options = KinopoiskMovie.objects.exclude(id=random_movie.id).filter().order_by('?')[:3]
-
-    context = {
-        'movie': random_movie,
-        'options': options,
-    }
+    return random_movie, options
 
 
-    return render(request, 'index.html', context)
+def index(request):
+    correct_answer, incorrect_answers = get_movie_and_options()
+    options = [correct_answer, *incorrect_answers]
+
+    return render(request, 'index.html', {'options': options})
