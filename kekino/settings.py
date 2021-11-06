2a9b89b85d167b35e2c36bb099bd89a599c1a5d4
n1k0din <nik726@gmail.com>
2021-11-06 18:25:16 +0700

    Update settings with media_root param

--- kekino/settings.py
+++ kekino/settings.py
@@ -107,8 +107,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
